3fdf4e5d1121c7787cc9c35b565b7a0549f3a5e4
Trexon25 <Trexon@gmx.ch>
2018-07-28 12:44:40 +0200

    project created
    Created a new Android Studio project.

---
+++ app/src/main/java/net/vaorra/spotifymultidevice/SMDMainActivity.java
@@ -0,0 +1,13 @@
+package net.vaorra.spotifymultidevice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SMDMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_smdmain);
+    }
+}
