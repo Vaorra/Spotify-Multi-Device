da9fae1885e423023100043604374d3a99655ece
koflin <colin.pfingstl@hotmail.ch>
2018-11-23 02:50:29 +0100

    23.11.2018 FINAL [STABLE]
    -added main menu
    -added join menu

--- app/src/main/java/net/vaorra/spotifymultidevice/SMDMainActivity.java
+++ app/src/main/java/net/vaorra/spotifymultidevice/SMDMainActivity.java
@@ -1,13 +1,50 @@
 package net.vaorra.spotifymultidevice;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class SMDMainActivity extends AppCompatActivity {
+
+    Button createButton;
+    Button joinButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_smdmain);
+
+        //Get UI Widgets
+        createButton = this.findViewById(R.id.create_room);
+        joinButton = this.findViewById(R.id.join_room);
+
+        //Register Events
+        registerEvents();
+    }
+
+    private void registerEvents(){
+
+        createButton.setOnClickListener(
+                new Button.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(v.getContext(), RoomActivity.class);
+                        startActivity(intent);
+                    }
+                }
+        );
+
+        joinButton.setOnClickListener(
+                new Button.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(v.getContext(), JoinRoomActivity.class);
+                        startActivity(intent);
+                    }
+                }
+        );
     }
 }
